013d2ea9fd9ca41cdeb81ba3e4cf21881f17e91f
Shamil Omarov <s_omarov05@mail.ru>
2022-03-25 01:16:53 +0300

    Сделал тесты под разные задачи

--- spec/task1.spec.js
+++ spec/task1.spec.js
@@ -2,6 +2,6 @@ const  { positiveSum } = require('../task1/task1');
 
 test('Sum of positive', function() {
   expect(positiveSum( [1, 2, 3, 4, 5] )).toBe(15);
+  expect(positiveSum( [-1, -2, -3] )).toBe(0);
   expect(positiveSum( [] )).toBe(0);
-  expect(positiveSum( [-1,-7,-9] )).toBe(0);
 });
